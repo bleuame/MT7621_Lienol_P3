c97f78b8e5aa8859856b928854840b29ff563e29
Five Rice <34707028+bleuame@users.noreply.github.com>
2022-06-18 22:49:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,10 +11,16 @@
 #
 
 # Uncomment a feed source
+# fw876/helloworld
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
+# Lienol/openwrt-package
 #sed -i '$a src-git laohu https://github.com/huchanghui123/Lienol-openwrt-packages-backup' feeds.conf.default
-echo 'src-git laohu https://github.com/huchanghui123/Lienol-openwrt-packages-backup' >>feeds.conf.default
+#echo 'src-git laohu https://github.com/huchanghui123/Lienol-openwrt-packages-backup' >>feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' feeds.conf.default
+
+# Add Lean source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
